88bf6f131f40974e0848b3932c5f41f75f2f00d9
Austin Wang <austin.wang0311@gmail.com>
2017-04-22 18:13:12 -0400

    Ga-ga-ga-ga-game!

--- js/index.js
+++ js/index.js
@@ -1,3 +1,32 @@
 function init() {
     var canvas = document.getElementById("game");
+    window.game = new Game(canvas);
+}
+
+var Game = function(canvas) {
+    this.canvas = canvas;
+    this.ctx = canvas.getContext('2d');
+
+    /* For pixel art */
+    /*
+    this.ctx.mozImageSmoothingEnabled = false;
+    this.ctx.webkitImageSmoothingEnabled = false;
+    this.ctx.msImageSmoothingEnabled = false;
+    this.ctx.imageSmoothingEnabled = false;
+    */
+    this.lastFrameTimeMs = 0;
+
+    this.gameLoop = (timestamp) => {
+        var delta = timestamp - this.lastFrameTimeMs;
+        this.lastFrameTimeMs = timestamp;
+        this.update(delta);
+        this.draw();
+        window.requestAnimationFrame(this.gameLoop);
+    }
+
+    this.update = (delta) => {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+
+    window.requestAnimationFrame(this.mainLoop);
 }
