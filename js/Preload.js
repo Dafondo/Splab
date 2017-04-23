4a4cf69d933e8bcbd1ac9e7ee10b93ee9ba6b55a
armaan <armaan.sethi@gmail.com>
2017-04-23 00:59:36 -0400

    attempt at a loading thing of sorts

--- js/Preload.js
+++ js/Preload.js
@@ -10,6 +10,7 @@ Splab.Preload.prototype = {
 
 		this.game.load.image('c', 'assets/sprites/c.png');
 		this.game.load.image('platform', 'assets/sprites/platform.png');
+
 	},
 
 	init: function() {
@@ -20,6 +21,41 @@ Splab.Preload.prototype = {
 	},
 
 	create: function() {
+        game.load.start();
+
 		this.state.start('MainMenu');
-	}
+	},
+    
+    loadStart: function() {
+        
+        text.setText("Loading ...");
+        
+    },
+    
+    //	This callback is sent the following parameters:
+    fileComplete: function(progress, cacheKey, success, totalLoaded, totalFiles) {
+        
+        text.setText("File Complete: " + progress + "% - " + totalLoaded + " out of " + totalFiles);
+        
+        var newImage = game.add.image(x, y, cacheKey);
+        
+        newImage.scale.set(0.3);
+        
+        x += newImage.width + 20;
+        
+        if (x > 700)
+        {
+            x = 32;
+            y += 332;
+        }
+        
+    },
+    
+    loadComplete: function() {
+        
+        text.setText("Load Complete");
+        
+    }
+
+
 };
