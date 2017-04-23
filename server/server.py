541c1fed92d20267847cf173253aa0ca6e1b81b0
Yicheng Wang <alex.wyc2098@protonmail.ch>
2017-04-22 23:52:39 -0400

    cleanup; add create and join logic in server

--- server/server.py
+++ server/server.py
@@ -20,15 +20,34 @@ def query_state():
         emit('connection', {'state': 'waiting'})
 
 @socketio.on("create")
-def create_game(claim):
-    pass
+def create_game(data):
+    global state
+    state = {}
+    state['capacity'] = data['capacity']
+    state['size'] = data['size']
+    if state['size'] == 'small': # TODO init sizes and shit
+        pass
+    state['players'] = []
+    state['players'].append((data['uname'], 0))
+
+    emit('created', data)
 
 @socketio.on("join")
-def join_request(claim):
-    pass
+def join_request(data):
+    global state
+    if len(state['players']) == state['capacity']: # room's full
+        return
+    joined_user = (data['uname'], len(state['players']))
+    state['players'].append(joined_user)
+
+    emit('join', {'uname': joined_user[0], 'id': joined_user[1]},
+            broadcast=True)
+    emit('joined', {'capacity': state['capacity'],
+                    'size': state['size'],
+                    'current_users': state['players']})
 
 @socketio.on("act")
-def act(claim):
+def act(data):
     pass
 
 if __name__ == '__main__':
