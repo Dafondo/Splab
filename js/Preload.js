34f0becdf56c49e8a265b3aa7f47a7444e424488
Austin Wang <austin.wang0311@gmail.com>
2017-04-23 01:57:06 -0400

    loading stuff for armaan is awesome

--- js/Preload.js
+++ js/Preload.js
@@ -4,58 +4,46 @@ var Splab = Splab || {};
 
 Splab.Preload = function() {};
 
+var text;
+
 Splab.Preload.prototype = {
 	loadAssets: function() {
 		// this.game.load.image('manly_chicken', 'assets/sprites/manly_chicken.jpg');
-
 		this.game.load.image('c', 'assets/sprites/c.png');
 		this.game.load.image('platform', 'assets/sprites/platform.png');
-
 	},
-
-	init: function() {
-	},
-
-	preload: function() {
-		this.loadAssets();
-	},
-
-	create: function() {
-        game.load.start();
-
-		this.state.start('MainMenu');
-	},
-    
     loadStart: function() {
-        
         text.setText("Loading ...");
-        
     },
-    
     //	This callback is sent the following parameters:
     fileComplete: function(progress, cacheKey, success, totalLoaded, totalFiles) {
-        
         text.setText("File Complete: " + progress + "% - " + totalLoaded + " out of " + totalFiles);
-        
+
         var newImage = game.add.image(x, y, cacheKey);
-        
+
         newImage.scale.set(0.3);
-        
+
         x += newImage.width + 20;
-        
+
         if (x > 700)
         {
             x = 32;
             y += 332;
         }
-        
     },
-    
     loadComplete: function() {
-        
         text.setText("Load Complete");
-        
-    }
-
-
+        console.log("chicken chicken");
+    },
+	init: function() {
+        this.load.onLoadStart.add(this.loadStart, this);
+        this.load.onLoadComplete.add(this.loadComplete, this);
+        text = this.add.text(32, 32, 'Click to start load', { fill: '#ffffff' });
+	},
+	preload: function() {
+		this.loadAssets();
+	},
+	create: function() {
+		this.state.start('MainMenu');
+	}
 };
